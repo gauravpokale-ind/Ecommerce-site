aa9145117295a5aadc56fe1e885958a7d9f12fb7
gauravpokale-ind <gauravpokale1126@gmail.com>
2024-09-29 18:58:47 +0530

    updated images for contact us and follow us

--- src/components/footerDetails/ContactUs.jsx
+++ src/components/footerDetails/ContactUs.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 function ContactUs() {
     return (
         <div>
-           <img src="src/assets/img/contactUs.jpg" alt="" style={{height:"500px", width:"500px", alignItems:"center" , justifyContent:"center", marginLeft:"500px" }} /> 
+           <img src="https://res.cloudinary.com/drah9v28t/image/upload/v1727615728/contactUs_yhyjah.jpg" alt="" style={{height:"500px", width:"500px", alignItems:"center" , justifyContent:"center", marginLeft:"500px" }} /> 
         </div>
     )
 }
--- src/components/footerDetails/FollowUs.jsx
+++ src/components/footerDetails/FollowUs.jsx
@@ -6,9 +6,9 @@ function FollowUs() {
         <div>
             <h3>folllow us on</h3>
             <div className="followus"  >
-                <a href="https://x.com" target="_blank"><img src="src/assets/img/X.png" alt="x" style={{height:"200px", width:"200px"}}  /></a>
-                <a href="https://facebook.com" target="_blank"><img src="src/assets/img/FB.png"  alt="fb" style={{height:"200px", width:"200px"}} /></a>
-                <a href="https://instagram.com" target="_blank"><img src="src/assets/img/Insta.png" alt="insta" style={{height:"300px", width:"300px"}} /></a>
+                <a href="https://x.com" target="_blank"><img src="https://res.cloudinary.com/drah9v28t/image/upload/v1727615739/X_ikhnv4.png" alt="x" style={{height:"200px", width:"200px"}}  /></a>
+                <a href="https://facebook.com" target="_blank"><img src="https://res.cloudinary.com/drah9v28t/image/upload/v1727615729/FB_htehxu.png"  alt="fb" style={{height:"200px", width:"200px"}} /></a>
+                <a href="https://instagram.com" target="_blank"><img src="https://res.cloudinary.com/drah9v28t/image/upload/v1727615732/Insta_bksq6l.png" alt="insta" style={{height:"300px", width:"300px"}} /></a>
             </div>
 
         </div>
