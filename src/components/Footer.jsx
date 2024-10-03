1c8dcbaa5c850a2fcc6e737586cc5c58e1e4a7cc
gauravpokale-ind <gauravpokale1126@gmail.com>
2024-10-03 20:32:32 +0530

    update css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ import { NavLink } from 'react-router-dom'
 function Footer() {
   return (
     <footer className='footerContainer'>
-      <NavLink to="/follow" ><p>Follow us</p></NavLink>
-      <NavLink to="/contact"><p>Contact Us</p></NavLink>
-      <NavLink to="/privacy"><p>Privacy Policy</p></NavLink>
-      <NavLink to="/terms"><p>Terms</p></NavLink>
-      <NavLink to="/volunteer"><p>Volunteer</p></NavLink>
-      <NavLink to="/partner"><p>Partner</p></NavLink>
-      <NavLink to="/api"><p>Free API</p></NavLink>
+      <NavLink to="/follow" className="footerTxt" ><p>Follow us</p></NavLink>
+      <NavLink to="/contact" className="footerTxt"><p>Contact Us</p></NavLink>
+      <NavLink to="/privacy" className="footerTxt"><p>Privacy Policy</p></NavLink>
+      <NavLink to="/terms" className="footerTxt"><p>Terms</p></NavLink>
+      <NavLink to="/volunteer" className="footerTxt"><p>Volunteer</p></NavLink>
+      <NavLink to="/partner" className="footerTxt"><p>Partner</p></NavLink>
+      <NavLink to="/api" className="footerTxt"><p>Free API</p></NavLink>
     </footer>
   )
 }
